68488676d4e18c7cd3466fd86cde66d0c15c4085
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-11 22:41:42 +0530

    get and post request done

--- app.js
+++ app.js
@@ -19,4 +19,19 @@ app.get("/", async (req, res) => {
   res.render("index", { shortUrls });
 });
 
+app.post("/shortUrl", async (req, res) => {
+  await shortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await shortUrl.findOne({ short: req.params.shortUrl });
+  if (!shortUrl) return res.redirect(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
