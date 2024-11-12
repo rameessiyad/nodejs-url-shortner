1bceb7d204bd99f02af7b95934cfc82c96d5781b
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-12 06:49:21 +0530

    fix: all errors fixed

--- app.js
+++ app.js
@@ -15,20 +15,22 @@ app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  const shortUrls = await shortUrl.find();
+  const shortUrls = await shortUrl.find({});
+  console.log(shortUrls);
   res.render("index", { shortUrls });
 });
 
-app.post("/shortUrl", async (req, res) => {
+app.post("/shortUrls", async (req, res) => {
   await shortUrl.create({ full: req.body.fullUrl });
+  console.log(req.body);
   res.redirect("/");
 });
 
 app.get("/:shortUrl", async (req, res) => {
-  const shortUrl = await shortUrl.findOne({ short: req.params.shortUrl });
-  if (!shortUrl) return res.redirect(404);
+  const shorturl = await shortUrl.findOne({ short: req.params.shortUrl });
+  if (!shorturl) return res.status(404).send("URL not found");
 
-  shortUrl.clicks++;
+  shorturl.clicks++;
   shortUrl.save();
 
   res.redirect(shortUrl.full);
